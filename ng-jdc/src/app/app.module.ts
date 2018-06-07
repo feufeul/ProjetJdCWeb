b8985c5e7e3a1795e74cdedb2d29254d8d27ed5d
feufeul <vanduffelchristopher@gmail.com>
2018-06-07 22:08:56 +0200

    Add the battleship_test project

---
+++ ng-jdc/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+    import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+	ToastModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
